b0201d9e4f38d45e6725ee36db45244aa49f294d
Jyodisen Sprenger <jyodisen_sprenger@sluz.ch>
2022-10-04 21:37:57 +0200

    + Scroll Colors / Header from 90px to 70px

--- #Code/js/index.js
+++ #Code/js/index.js
@@ -5,7 +5,7 @@ function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("header").style.fontSize = "30px";
   } else {
-    document.getElementById("header").style.fontSize = "90px";
+    document.getElementById("header").style.fontSize = "70px";
   }
 }
 
